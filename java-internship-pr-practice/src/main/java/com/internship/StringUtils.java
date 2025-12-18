06aea9ccf80c82931d18678b326c569e2359892d
Akshata <akshataulbhagat@.com>
2025-12-18 21:03:12 +0530

    Fix:reverse method

--- java-internship-pr-practice/src/main/java/com/internship/StringUtils.java
+++ java-internship-pr-practice/src/main/java/com/internship/StringUtils.java
@@ -2,8 +2,9 @@ package com.internship;
 
 public class StringUtils {
 
-    // ISSUE: Method should reverse the string
+    // Fix: Method reverses the string
     public static String reverse(String input) {
-        return input;
+        if (input == null) return null; // handle null input
+        return new StringBuilder(input).reverse().toString();
     }
 }
